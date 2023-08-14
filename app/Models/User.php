e58204c08450e99da31a485a026659c7560bc0e3
root <root@1PC10O101P22.uautonoma.local>
2023-08-14 18:44:33 -0400

    Creacion de recursos, estos son importantes y es donde se aplica la logica RESTful

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Builder;
 
 class User extends Authenticatable
 {
@@ -42,4 +43,23 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+
+
+
+   
+
+    public function scopeCustomers(Builder $builder): Builder
+    {
+        return $builder->where('is_customer', true);
+    }
+    
 }
+
+
+
+
+
+
+
